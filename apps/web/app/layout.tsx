6d9aad77273bba1a14d68bbbf86c5ce73001a8b2
jamc96 <jamc.mejia@gmail.com>
2025-02-20 08:32:54 -0600

    feat: maintenance mode to redisign

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import NavigationBar from '@/components/navigation-bar';
-import Footer from '@/components/footer';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -33,11 +31,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased relative flex flex-col min-h-screen`}
       >
-        <NavigationBar />
         <main className='flex-1 bg-black flex flex-col items-center justify-center'>
           {children}
         </main>
-        <Footer />
       </body>
     </html>
   );
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,9 @@
-import FeaturedProjects from '@/components/sections/featured-projects';
-import Hero from '@/components/sections/hero';
+import ComingSoonText from '@/components/elements/coming-soon-text';
 
 export default function Home() {
   return (
-    <div className='flex flex-col w-full'>
-      <Hero />
-      <FeaturedProjects />
+    <div className='flex flex-col w-full items-center justify-center'>
+      <ComingSoonText />
     </div>
   );
 }
